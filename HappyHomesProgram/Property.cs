0f47a8caa25e90bcc5677daee13db5898e3b6716
ric22000863 <ric22000863@btc.ac.uk>
2025-04-28 15:51:32 +0100

    Added properties to the Property class

--- HappyHomesProgram/Property.cs
+++ HappyHomesProgram/Property.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace HappyHomesProgram
 {
-    internal class Property
+    // Represents a property that is for sale
+    class Property
     {
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public string Type { get; set; } // detached, semi-detached, flat
+
+        public Property(int id, string address, string type)
+        {
+            Id = id;
+            Address = address;
+            Type = type;
+        }
     }
 }
