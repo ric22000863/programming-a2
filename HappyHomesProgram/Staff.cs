dd889b3a7a7b68ac41cdf57d8f4cd6a6ff634283
ric22000863 <ric22000863@btc.ac.uk>
2025-04-29 09:38:54 +0100

    Added properties to the Staff class

--- HappyHomesProgram/Staff.cs
+++ HappyHomesProgram/Staff.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace HappyHomesProgram
 {
-    internal class Staff
+    // Class to represent a staff member
+    class Staff
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<DateTime> UnavailableTimes { get; set; } = new List<DateTime>();
+
+        public Staff(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        // Check if staff is available at a specific time
+        public bool IsAvailable(DateTime dateTime)
+        {
+            return !UnavailableTimes.Contains(dateTime);
+        }
+
+        // Mark staff as unavailable at a specific time
+        public void MarkUnavailable(DateTime dateTime)
+        {
+            UnavailableTimes.Add(dateTime);
+        }
     }
 }
