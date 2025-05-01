90ea1e10bc0f23c74a8d0852fe91e2ebb56b49b8
ric22000863 <ric22000863@btc.ac.uk>
2025-05-01 02:15:54 +0100

    Added the AddCustomer(), AddBooking() and AddStaff() methods

--- HappyHomesProgram/Program.cs
+++ HappyHomesProgram/Program.cs
@@ -13,7 +13,7 @@ int nextStaffId = 1;
 int nextViewingId = 1;
 
 
-Console.WriteLine("Welcome to HappyHomes Property Viewing System!");
+Console.WriteLine("Welcome to HappyHomes Property Viewing System");
 Console.WriteLine("Please select an option:");
 Console.WriteLine("1. Add a Customer");
 Console.WriteLine("2. Add a Property");
@@ -31,6 +31,7 @@ switch (choice)
 {
     case 1:
         // Call add customer method
+        AddCustomer();
         break;
 
     case 2:
@@ -43,7 +44,8 @@ switch (choice)
         break;
 
     case 4:
-        // Call add booking method
+        // Call method to book a viewing
+        AddBooking();
         break;
     
     case 5:
@@ -67,8 +69,10 @@ switch (choice)
         break;
 }
 
+// Method to store property details
 void AddProperty()
 {
+    // User enters the details of the property
     Console.Write("Enter property address: ");
     string address = Console.ReadLine();
     Console.Write("Enter property type (Detached, Flat, etc.): ");
@@ -77,5 +81,75 @@ void AddProperty()
     Property property = new Property(nextPropertyId++, address, type);
     properties.Add(property);
 
-    Console.WriteLine($"Property added successfully! \n Property ID: {property.Id}");
+    Console.WriteLine($"Property added successfully. \nProperty ID is: {property.Id}");
+}
+
+// Method to store customer details
+void AddCustomer()
+{
+    // User enters the cuatomer details
+    Console.Write("Enter customer name: ");
+    string name = Console.ReadLine();
+    Console.Write("Enter customer email: ");
+    string email = Console.ReadLine();
+    Console.Write("Enter customer current address: ");
+    string currentAddress = Console.ReadLine();
+    Console.Write("Enter customer telephone: ");
+    string telephone = Console.ReadLine();
+
+    Customer customer = new Customer(nextCustomerId++, name, email, currentAddress, telephone);
+    customers.Add(customer);
+    Console.WriteLine($"Customer details added successfully. \nCustomer ID is: {customer.Id}");
+}
+
+// Method to book a viewing
+void AddBooking()
+{
+    // User enters the customer ID for the booking
+    Console.Write("Enter customer ID: ");
+    int customerId = Convert.ToInt32(Console.ReadLine());
+    Customer customer = customers.FirstOrDefault(c => c.Id == customerId);
+    if (customer == null)
+    {
+        // If customer not found, display message
+        Console.WriteLine("Customer not found.");
+        return;
+    }
+
+    // User enters the property ID for the booking
+    Console.Write("Enter property ID: ");
+    int propertyId = Convert.ToInt32(Console.ReadLine());
+    Property property = properties.FirstOrDefault(p => p.Id == propertyId);
+    if (property == null)
+    {
+        Console.WriteLine("Property not found.");
+        return;
+    }
+    // User enters the staff ID for the booking
+    Console.Write("Enter staff ID: ");
+    int staffId = Convert.ToInt32(Console.ReadLine());
+    Staff staff = staffMembers.FirstOrDefault(s => s.Id == staffId);
+    if (staff == null)
+    {
+        // If the staff member isn't found, display message
+        Console.WriteLine("Staff member not found.");
+        return;
+    }
+    // User inputs the date and time for the booking
+    Console.Write("Enter date and time for viewing (yyyy-mm-dd hh:mm): ");
+    DateTime dateTime = DateTime.Parse(Console.ReadLine());
+    Viewing viewing = new Viewing(nextViewingId++, customer, property, staff, dateTime);
+    viewings.Add(viewing);
+    Console.WriteLine($"Viewing booked successfully. \nViewing ID is: {viewing.Id}");
+}
+
+// Method to add staff member
+void AddStaff()
+{
+    // User enters the staff member details
+    Console.Write("Enter staff name: ");
+    string name = Console.ReadLine();
+    Staff staff = new Staff(nextStaffId++, name);
+    staffMembers.Add(staff);
+    Console.WriteLine($"Staff member added successfully. \nStaff ID is: {staff.Id}");
 }
